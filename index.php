f080475d238eca40cf693fceb0a8b76c9763b6ac
habib <habibokz118@gmail.com>
2024-01-25 08:06:04 -0800

    calculator

--- index.php
+++ index.php
@@ -1,40 +1,51 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
-    $a=$_GET['num1'];
-    $c=$_GET['num2'];
-    $b=$_GET['num3'];
-// if($c=="+"){
-//     echo $a +$b;
-// }
-// elseif($c=="-"){echo $a - $b;}
-// elseif($c=="*"){echo $a * $b;}
-// else{
-//     echo "The symbol is incorrect";
-// }
-    switch($c){
+    <?php
+    $a = $_GET['num1'];
+    $c = $_GET['num2'];
+    $b = $_GET['num3'];
+    // if($c=="+"){
+    //     echo $a +$b;
+    // }
+    //  elseif($c=="-"){echo $a - $b;}
+    // elseif($c=="*"){echo $a * $b;}
+    // elseif($c=="/"){echo $a / $b;}
+    // elseif($c=="**"){echo $a ** $b;}
+    // elseif($c=="sqt"){echo (sqrt($c));}
+    // else{
+    //     echo "The symbol is incorrect";
+    //  }
+    switch ($c) {
         case "+":
-            echo $a +$b;
+            echo $a + $b;
             break;
         case "-":
-            echo $a -$b;
+            echo $a - $b;
             break;
         case "*":
-            echo $a *$b;
+            echo $a * $b;
             break;
-            default:echo "the symble is incorrect";
-
+        case "/":
+            echo $a / $b;
+            break;
+        case "**":
+            echo $a ** $b;
+            break;
+        case "sqt":
+            echo(sqrt($c));
+            break;
+        default:
+            echo "the symble is incorrect";
     }
-    
-
-      ?>
+    ?>
 </body>
-</html>
 
+</html>
